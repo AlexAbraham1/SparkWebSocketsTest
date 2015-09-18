3839ff81f3c7996cb3d8f882aa095e5c8f70a7ed
Alex Abraham <alex@abraham.net>
2015-09-18 15:07:20 -0400

    Added main class

---
+++ src/net/alexabraham/sparkjava/websockets/Main.java
@@ -0,0 +1,11 @@
+package net.alexabraham.sparkjava.websockets;
+
+/**
+ * Created by alexabraham on 9/18/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hi!");
+    }
+}
